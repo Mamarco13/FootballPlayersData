5ef67de5734b2cbcbeacb9f24c417f7b57720c99
Manuel <mamarco1308@gmail.com>
2025-01-09 01:16:20 +0100

    Forzar puerto 8000

--- AbrirApi.sh
+++ AbrirApi.sh
@@ -9,9 +9,5 @@ poetry install --no-root
 # (Opcional) Agregar el directorio actual al PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
-# Iniciar el servidor de la API usando poetry run
-poetry run python app/main.py &
-
-
-
-
+# Iniciar el servidor de la API usando poetry run, forzando el puerto 8000
+poetry run python app/main.py --port 8000 &
